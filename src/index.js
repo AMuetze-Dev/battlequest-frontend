9306cc7a43fc28e3555f5167045dc155e4cd4f70
Wayssm <aaron.muetze@outlook.com>
2023-08-28 21:31:58 +0200

    lena push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Login from "./components/Login/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Preferences from "./components/Preferences/Preferences";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+function setToken(userToken) {
+  sessionStorage.setItem("token",JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem("token");
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
+function App() {
+
+  const token = getToken();
+
+  return (
+    <div className="wrapper">
+      {!token && <Login setToken={setToken} />}
+      {token &&
+        <React.Fragment>
+          <h1>Application</h1>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/preferences" element={<Preferences />} />
+            </Routes>
+          </BrowserRouter>
+        </React.Fragment>
+      }
+    </div>
+  )
+}

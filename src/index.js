3dbf705469692de97c90ec7c80824fd7bf764d58
Wayssm <aaron.muetze@outlook.com>
2023-09-01 16:26:50 +0200

    backup

--- src/index.js
+++ src/index.js
@@ -1,43 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Login from "./components/Login/Login";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Preferences from "./components/Preferences/Preferences";
-
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-
-function setToken(userToken) {
-  sessionStorage.setItem("token",JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem("token");
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
-
-function App() {
-
-  const token = getToken();
-
-  return (
-    <div className="wrapper">
-      {!token && <Login setToken={setToken} />}
-      {token &&
-        <React.Fragment>
-          <h1>Application</h1>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/preferences" element={<Preferences />} />
-            </Routes>
-          </BrowserRouter>
-        </React.Fragment>
-      }
-    </div>
-  )
-}
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
